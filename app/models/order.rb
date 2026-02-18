7b9d294fa522a0a264c66166cdb7675389b06ec2
James Mwangi K <jameshmwangiy@gmail.com>
2026-02-18 11:48:56 +0300

    fix: Ensure all item quantity updates in Order#update_total_quantity are within a single transaction.

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,10 +6,11 @@ class Order < ApplicationRecord
 
   def update_total_quantity
     ActiveRecord::Base.transaction do
-    self.ordered_lists.each do |line_item|
-      item = Item.lock.find(line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
+      self.ordered_lists.each do |line_item|
+        item = Item.lock.find(line_item.item_id)
+        item.total_quantity += line_item.quantity
+        item.save!
+      end
     end
   end
 end
